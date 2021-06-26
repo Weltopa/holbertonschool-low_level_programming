d93dca97c2e26aa2758c004639a9498d2d569e0f
Patrick Welton <3447@holbertonschool.com>
2021-06-25 23:57:59 -0400

    alpha search

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	x = 'a';
+	while (x <= 'z')
+	{
+		if (x != 'e' && x != 'q')
+		putchar(x);
+		x++;
+	}
+		putchar('\n');
+	return (0);
+}
