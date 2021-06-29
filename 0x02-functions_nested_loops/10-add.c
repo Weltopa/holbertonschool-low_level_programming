fba25d96e817f311c102dbb990da1cc52cd6401a
Patrick Welton <3447@holbertonschool.com>
2021-06-29 11:11:26 -0400

    Adding 2 variables

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - adds two variables.
+ *
+ * @a: First add variable
+ * @b: Variable for other input.
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
