bcdce01ca2014d2fc1ea14a4ccdd2d481efc7547
Patrick Welton <3447@holbertonschool.com>
2021-07-01 22:42:01 -0400

    Function that checks for uppe case letters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _isupper - Function that checks upper case letters.
+ * @c: Taking input for function.
+ *Return: Return 1 or 0
+ */
+int _isupper(int c)
+{
+	int upper, out;
+
+	out = 0;
+
+	for (upper = 'A'; upper <= 'Z'; upper++)
+	{
+		if (c == upper)
+		out = 1;
+	}
+	return (out);
+}
