95aaac9ce7fccaa9776c71914f84728525355f96
Patrick Welton <3447@holbertonschool.com>
2021-07-06 17:53:07 -0400

    every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * puts2 - print every other character of string.
+ * @str: Pointed variable
+ * Return: Never
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (i = 0; i < _strlen(str); i += 2)
+	{
+		_putchar(input[i]);
+	}
+	_putchar('\n');
+}
+/**
+ * _strlen - Function that returns length of a string.
+ * @s: Pointed variable
+ * Return: pointed variable
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (*s != '\0')
+	{
+		c++;
+		s++;
+	}
+	return (c);
+}
