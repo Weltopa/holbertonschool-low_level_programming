ee2621c0318b2f02921d91f5e416adc0232d9817
Patrick Welton <3447@holbertonschool.com>
2021-07-08 23:14:08 -0400

    strncopy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strncat - Concataenatates two string by n bytes.
+ * @src: copied string
+ * @dest: string added to
+ * @n: number of characters to append.
+ * Return: dest string.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
