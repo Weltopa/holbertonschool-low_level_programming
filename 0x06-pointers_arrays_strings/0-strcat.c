d50a6fdd126c627896207b7d158290bc516c23ad
Patrick Welton <3447@holbertonschool.com>
2021-07-08 22:26:21 -0400

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * _strcat - Concataenatates two string.
+ * @src: added string
+ * @dest: string added to
+ * Return: concatentated string.
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest + _strlen(dest);
+
+	while (*ptr != '\0')
+	{
+		*ptr++ = *src++;
+	}
+	*ptr = '\0';
+	return (dest);
+}
+
+/**
+ * _strlen - Function that returns length of a string.
+ * @s: Pointed variable
+ * Return: pointed variable
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (*s != '\0')
+	{
+		c++;
+		s++;
+	}
+	return (c);
+}
