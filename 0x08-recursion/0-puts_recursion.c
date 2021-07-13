eb264702370e5e7ad25b6ab802ac22f93cf2a51f
Patrick Welton <3447@holbertonschool.com>
2021-07-12 20:42:30 -0400

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,13 +5,15 @@
  * @s: Pointed string
  * Return: Never
  */
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-	_putchar(*s++);
-	_puts_recursion(s);
+		_putchar(*s++);
+		_puts_recursion(s);
 	}
 	else
-	_putchar('\n')
+	{
+		_putchar('\n');
+	}
 }
