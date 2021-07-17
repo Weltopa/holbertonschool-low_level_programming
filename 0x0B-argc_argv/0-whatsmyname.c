54e6a40fdab11c20ab5e80f52b42907630edd858
Patrick Welton <3447@holbertonschool.com>
2021-07-16 21:30:34 -0400

    program that prints a name

---
+++ 0x0B-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * Main - Print name of program.
+ * @argc: number of string.
+ * @argv: pointer.
+ * Return: never.
+ */
+int main(int argc, char *char *argv[])
+{
+	if (argc >= 0)
+		printf("%s\n", argv);
+	return (0);
+}
