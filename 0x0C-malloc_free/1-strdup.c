5838ef96a57eaee33f037d848b95827008f40da1
Patrick Welton <3447@holbertonschool.com>
2021-07-22 09:47:15 -0400

    malloc copy

---
+++ 0x0C-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+Char *_strdup(char *str)
+{
+	char *new_Str;
+	int i;
+	int length = 0;
+
+	if (!str)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		length++;
+
+	new_str = malloc(sizeof(char) * length));
+
+	if (!new_str)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		new_str[i] = str[i]
+
+	new_str[length] = '\0';
+
+	return (new_str);
+}
