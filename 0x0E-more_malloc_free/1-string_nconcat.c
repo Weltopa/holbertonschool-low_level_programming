8f002176deaf7e0d9fb8ebb0444d86da18db8026
Patrick Welton <3447@holbertonschool.com>
2021-07-23 16:20:26 -0400

    concatenate skip

--- 0x0E-more_malloc_free/1-string_nconcat.c
+++ 0x0E-more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		size2++;
 	if (n < size2)
 		size2 = n;
-	con_str = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
+	con_str = malloc(sizeof(char) * (strlen(s1) + n + 1));
 	if (con_str == NULL)
 		return (NULL);
 
