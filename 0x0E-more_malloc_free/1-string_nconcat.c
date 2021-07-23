6d08cde06dbab12d64fdbfd1ba6935bfd6f732bf
Patrick Welton <3447@holbertonschool.com>
2021-07-23 16:07:08 -0400

    concatenate skip

--- 0x0E-more_malloc_free/1-string_nconcat.c
+++ 0x0E-more_malloc_free/1-string_nconcat.c
@@ -36,5 +36,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		con_str[i] = s1[i];
 	for (x = 0; s2[x] != '\0' && x < n; x++, i++)
 		con_str[i] = s2[x];
+	con_str[i] = '\0';
 	return (con_str);
 }
