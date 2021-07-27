c55a6d1ee086285436a4a902fa15022989194ab9
Weltopa <patrick.welton@yahoo.com>
2021-07-26 20:01:05 -0700

    edits for checks

--- 0x0F-structures_typedef/1-init_dog.c
+++ 0x0F-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-#include <stdlib>
+#include <stdlib.h>
 /**
  * init_dog - initializes a variable of type struct dog.
  * @d: pointer.
--- 0x0F-structures_typedef/dog.h
+++ 0x0F-structures_typedef/dog.h
@@ -6,14 +6,14 @@
  * @age: dog age.
  * @owner: dog owner name.
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog;
+} dog;
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+
 #endif
