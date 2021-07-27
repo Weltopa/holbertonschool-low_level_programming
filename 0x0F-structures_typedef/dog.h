e5c04418c2393cd89f3798f9394890b811eec63e
Weltopa <patrick.welton@yahoo.com>
2021-07-26 19:38:16 -0700

    Dog Header

--- 0x0F-structures_typedef/dog.h
+++ 0x0F-structures_typedef/dog.h
@@ -1,18 +1,17 @@
 #ifndef _DOG_H
 #define _DOG_H
 /**
- * struct dog - Function that initializes a variable of type struct dog.
+ * struct dog - info re: dog
  * @name: Dog name.
  * @age: dog age.
  * @owner: dog owner name.
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
