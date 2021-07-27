7030391330d5cb61d42e05c99b8ff8e5761a4761
Weltopa <patrick.welton@yahoo.com>
2021-07-26 19:11:35 -0700

    struct dog

---
+++ 0x0F-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - Function that initializes a variable of type struct dog.
+ * @name: Dog name.
+ * @age: dog age.
+ * @owner: dog owner name.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
