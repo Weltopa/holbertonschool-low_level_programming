0f447a204000def9f21a5187ba35f382fd284cd7
Weltopa <patrick.welton@yahoo.com>
2021-07-27 18:51:59 -0700

    edit to print

--- 0x10-function_pointers/0-print_name.c
+++ 0x10-function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
 /**
  * print_name - print the name.
  * @name: the name.
- * @f: pointer to the funtion.
+ * @f: pointer to the function.
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
