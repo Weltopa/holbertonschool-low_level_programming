056698687b8e57382290a9952235d406febd45ef
Patrick Welton <3447@holbertonschool.com>
2021-07-29 23:55:54 -0400

    printf

---
+++ 0x11-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+/**
+ * print_strings - print strings.
+ * @separator: character to seperate.
+ * @n: number of arguments.
+ * Return: void.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list string_list;
+	unsigned int i;
+	char *string;
+
+	va_start(string_list, n);
+	for (i = 0; i < n; i++)
+	{
+		string = va_arg(string_list, char *);
+		if (string != NULL)
+			printf("%s", string);
+		else
+			printf("%p", string);
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(string_list);
+}
