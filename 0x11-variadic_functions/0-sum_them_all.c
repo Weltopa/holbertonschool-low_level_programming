3935ece58917ad5d1f14f3a7f3bd7277199fb955
Patrick Welton <3447@holbertonschool.com>
2021-07-29 22:48:54 -0400

    sum return

--- 0x11-variadic_functions/0-sum_them_all.c
+++ 0x11-variadic_functions/0-sum_them_all.c
@@ -10,14 +10,12 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list valist;
-	unsigned int sum = 0;
+	int sum = 0;
 	unsigned int x;
 
 	if (n == 0)
 		return (0);
-
 	va_start(valist, n);
-
 	for (X = 0; x < n; x++)
 		sum += va_arg(valist, int);
 	va_end(valist);
