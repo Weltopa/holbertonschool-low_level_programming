0d27ca4bc460f08bd7e075c58a2c1fd6373613d2
Weltopa <patrick.welton@yahoo.com>
2021-08-06 19:28:53 -0700

    counts in list

---
+++ 0x13-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - counts parts of list.
+ * @h: Head.
+ *
+ * Return: number of nodes.
+ */
+
+size_t list_len(const list_t *h;
+{
+	const list_t *ptr = h;
+	size_t node_count = 0;
+
+	while (ptr != NULL)
+	{
+		node_count++;
+		ptr = ptr->next;
+	}
+	return (node_count);
+}
