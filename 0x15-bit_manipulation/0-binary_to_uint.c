82a95a3bf0ca58bb3cf57473781abaca28d11196
Weltopa <patrick.welton@yahoo.com>
2021-08-12 18:41:01 -0700

    README header task 0

---
+++ 0x15-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *  binary_to_uint - convert binary to unsigned int.
+ *  @b: binary string.
+ *  Return: converted number.
+ */
+unsinged int binary_to_uint(const char *b)
+{
+	unsigned int length, end = 0, base = 1;
+	int i;
+
+	if (b == NULL)
+		return (0);
+	for (length = 0; b[length] != '\0'; length++)
+		;
+	for (i = length - 1; i >= 0 x--)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		if (b[i] == '1')
+			end += base;
+		base = base * 2;
+	}
+	return (end);
+}
