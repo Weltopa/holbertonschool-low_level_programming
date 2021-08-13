f07d443a56c4ebaf49f0062103f2f36d1590eb5c
Weltopa <patrick.welton@yahoo.com>
2021-08-12 19:38:40 -0700

    flip slim shady

---
+++ 0x15-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * flip_bits - flip m and n.
+ * @n: first number.
+ * @m: second number.
+ * Return: flip bits.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int end = 0;
+
+	while (n > 0 || m > 0)
+	{
+		if ((n & 1))
+			end++;
+		m = m >> 1;
+		n = n >> 1;
+	}
+	return (end);
+}
