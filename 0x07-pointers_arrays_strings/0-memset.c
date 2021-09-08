48115c2d4cff03463dd8ea8daf9d1cbc75481c90
Weltopa <patrick.welton@yahoo.com>
2021-09-07 20:37:39 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _memset - Fills memory with constant byte.
+ * @s: pointer
+ * @b: constant
+ * @n: number of bytes
+ * Return: pointer to s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
