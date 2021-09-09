438f2886cfd65c3e989e10a135b2553c3a42222a
Weltopa <patrick.welton@yahoo.com>
2021-09-08 17:58:51 -0700

    dlistint_len

---
+++ 0x18-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * dlistint_len - length of doubly linked list.
+ * @h: pointer to the head of the list.
+ * Return: # of nodes in list.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *current = NULL;
+	unsigned int node = 0;
+
+	if (h)
+	{
+		current = h;
+		while (current)
+		{
+			node++;
+			current = current->next;
+		}
+	}
+	return (node);
+}
