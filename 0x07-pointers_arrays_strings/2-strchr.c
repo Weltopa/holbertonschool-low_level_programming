58ea80bb27f1fa0497e671b811c6abf464d332bc
Weltopa <patrick.welton@yahoo.com>
2021-09-10 14:53:40 -0700

    return pointer

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#inlcude "holberton.h"
+/**
+ * _strchr - Locates character in string.
+ * @s: pointer
+ * @c: character
+ * Return: string
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != c)
+		if (!*s++)
+			return (NULL);
+	return (s);
+}
